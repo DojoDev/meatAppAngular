4b64753b327d8d2848b652f7ade27c497eb5b9fb
DojoDev <dojodevbr@gmail.com>
2018-07-08 08:24:15 -0300

    Injectble reviews in Reviews Temaplate

--- meat-app-starter/src/app/restaurants/restaurant/restaurant.service.ts
+++ meat-app-starter/src/app/restaurants/restaurant/restaurant.service.ts
@@ -1,8 +1,8 @@
 import { ErrorHandler } from './../../app.error-handler';
 
-import{Injectable} from'@angular/core';
+import { Injectable } from '@angular/core';
 
-import{Http} from '@angular/http';
+import { Http } from '@angular/http';
 
 import { MEAT_API } from './../../app.api';
 
@@ -10,23 +10,37 @@ import { Restaurant } from './restaurant.model';
 
 import { Observable } from 'rxjs/Observable';
 
-import'rxjs/add/operator/map'
-import'rxjs/add/operator/catch'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 @Injectable()
-export class RestaurantsService{
+export class RestaurantsService {
 
-    constructor(private http: Http){}
-    
-   restaurants(): Observable <Restaurant[]> {
+  constructor(private http: Http) { }
+  /*
+  *Tratamento de insjeçaõ http para busca restaurantes
+  */
+  restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
-    .map(response=>response.json())
-    .catch(ErrorHandler.handleError)
-   }
- restaurantById(id: string): Observable<Restaurant>{
-     return this.http.get(`${MEAT_API}/restaurants/${id}`)
-     .map(response=>response.json())
-     .catch(ErrorHandler.handleError)
- }
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+  /*
+  *Tratamento de insjeção http para busca restaurantes tratar e buscar
+  *para buscar o parametro de id de cada restaurante individialmente
+  */
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+  /*
+  *Tratamento de insjeção http para busca reviews de cada restaurante
+  */
+  reviewsOfRestaurant(id: string) {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(resolve => resolve.json())
+      .catch(ErrorHandler.handleError)
+  }
 
 }
